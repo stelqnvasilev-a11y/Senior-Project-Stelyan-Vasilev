94094657c4f459e4a4a5b0a19c34d5773d9bd15c
Stelyan Vasilev <stelqnvasilev@icloud.com>
2026-02-08 14:26:31 +0200

    Initial room selector logic

---
+++ src/RoomSelector.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class RoomSelector {
+
+    public static String getFloorRoomNumber(String input) {
+        String room = input.trim();
+
+        if (!room.matches("\\d{1,4}[A-Za-z]?")) {
+            return "Invalid format of the room, it can only consist of up to 4 digits and 1 letter max";
+        } else if (room.contentEquals("Red Room")) {
+            return "The Red Room is at the 4th floor";
+        }
+        int floor;
+        //If the room number is 1 digit ( or if it has a letter (like our 4a and 4b))//
+        if (room.length()==1 || (room.length() == 2 && Character.isLetter(room.charAt(1)))){
+            floor = 1;
+        } else {
+            floor = Character.getNumericValue(room.charAt(0));
+        }
+        return "Room " + room + " is on Floor " + floor;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What room are you looking for in Main Building");
+        String input = sc.nextLine();
+        System.out.println(getFloorRoomNumber(input));
+        sc.close();
+
+    }
+}
