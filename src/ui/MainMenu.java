bce5c6223d6b71aee540ca24ae12ae3ee2ee0e11
Stelyan Vasilev <stelqnvasilev@icloud.com>
2026-04-16 17:59:11 +0300

    Main menu updated with dfs prerequisites functionality and working

--- src/ui/MainMenu.java
+++ src/ui/MainMenu.java
@@ -2,6 +2,10 @@ package ui;
 
 import features.MainBuildingRoomHelp;
 import features.courses.CoursePlanner;
+import features.courses.CoursePrerequisiteFinder;
+import features.courses.CourseGraph;
+import features.courses.CourseLoader;
+import java.util.List;
 
 import java.util.Scanner;
 
@@ -19,6 +23,8 @@ public class MainMenu {
 
                 case 2 -> new CoursePlanner(sc).run();
 
+                case 3 -> showPrerequisites();
+
                 case 0 -> {
                     System.out.println("Goodbye!");
                     return;
@@ -35,6 +41,7 @@ public class MainMenu {
         System.out.println("... UNIVERSITY GUIDE ...");
         System.out.println("1) I need help to get to my room in Main Building");
         System.out.println("2) Show valid course order");
+        System.out.println("3) Show prerequisites for a course");
         System.out.println("0) Exit");
     }
 
@@ -48,5 +55,44 @@ public class MainMenu {
                 System.out.println("Please enter a number.");
             }
         }
+    }
+
+    private void showPrerequisites() {
+        try {
+            System.out.print("Enter course: ");
+            String course = sc.nextLine().trim().toUpperCase();
+
+            if (course.isEmpty()) {
+                System.out.println("Course cannot be empty.");
+                return;
+            }
+
+            CourseGraph graph = CourseLoader.loadGraph(
+                    "src/data/courses.csv",
+                    "src/data/prerequisites.csv",
+                    "src/data/conditions.csv"
+            );
+
+            List<String> prereqs =
+                    CoursePrerequisiteFinder.getAllPrerequisites(graph, course);
+
+            System.out.println();
+            System.out.println("Prerequisites for " + course + ":");
+
+            if (prereqs.isEmpty()) {
+                System.out.println("No prerequisites found.");
+            } else {
+                for (String p : prereqs) {
+                    System.out.println("- " + p);
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error loading data: " + e.getMessage());
+        }
+
+        System.out.println();
+        System.out.print("Press ENTER to go back...");
+        sc.nextLine();
     }
 }
